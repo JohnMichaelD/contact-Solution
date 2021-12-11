f4ddcb3d9dc5a36b33cddd4562c019b5cc4f9ca2
John Darrin <john.m.darrin@gmail.com>
2021-12-10 19:21:34 -0500

    css animations, pseudocode

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-//Pseudocode for counter functions
-//save current date into variable dateStarted
-//grab current date = let that equal currentDate
-//run function to compare dateStarted with currentDate, save difference as count, Lcount, Rcount
-//if dates are equal, do nothing to date
-    //apply counts from buttons
-//if dates are not equal, 
-
-
-// left lens counter display
 let lCountDisplayElem = document.querySelector(".lCountDisplay");
 let rCountDisplayElem = document.querySelector(".rCountDisplay");
-//glasses counter
 let counterMinusElem = document.querySelector(".buttons.counterMinus");
-//slept in counter
 let counterPlusElem = document.querySelector(".buttons.counterPlus");
-
-let leftResetButtonElem = document.querySelector("#refreshButtonL");
-
+let leftResetButtonElem = document.querySelector("#refreshButtonL"); 
 let rightResetButtonElem = document.querySelector("#refreshButtonR");
 
 //initial count for left lens counter
@@ -24,55 +10,91 @@ let lCount = 0;
 //initial count for right lens counter
 let rCount = 0;
 //initial count for lens case counter
-//let caseCount = 0;
+let caseCount = 0;
 
 updateCountDisplay();
-console.log(lCount, rCount);
 
-setTimeout(time, 3000);
-
-function time() {
-    lCount++;
-}
-
+//when slept-in button is pressed, add 1 to each contact lens count
 counterPlusElem.addEventListener('click', () => {
     lCount++;
     rCount++;
     updateCountDisplay();
 });
 
+//when glasses button is pressed, subtract 1 from each contact lens count
 counterMinusElem.addEventListener('click', () => {
     lCount--;
     rCount--;
     updateCountDisplay();
 });
 
+//when called, function updates the display to show correct counters
 function updateCountDisplay(){
     lCountDisplayElem.innerHTML = lCount;
     rCountDisplayElem.innerHTML = rCount;
     return 0;
 };
 
+//left contact counter is reset to zero, when left reset button is clicked
 leftResetButtonElem.addEventListener('click', () => {
     lCount = 0;
     updateCountDisplay();
 })
 
+//right contact counter is reset to zero, when right reset button is clicked
 rightResetButtonElem.addEventListener('click', () => {
     rCount = 0;
     updateCountDisplay();
 })
 
 
+// PSEUDOCODE_________________
+// first, check if user is new, (not signed in?) or has already started the counter previously.
+
+// if new, 
+    // alert, 'hello, press here to begin count' (should it be a countdown?)
+    // prompt user to enter days of current contact usage; lInput, rInput
+        // lInput = lCount;
+        // rInput = rCount;
+    // begin counter, save timestamp to variable??? use cookies? cache? or database?
+        //grab current date/timestamp = let that equal startDate
+
+// if not new, update UI to show counter for user using startDate
+    // run function to compare startDate with currentDate, save difference as count, lCount, rCount
+    // loop this function? how often should it be updating?
+
+
+// (ALERT) if lCount or/and rCount = 30 
+    // alert user
+    // prompt reset button, "Ready to change your contacts? This will reset the counters back to zero."
+        // if yes
+            // lCount = 0
+            // rCount = 0
+        // if no 
+            // do nothing
+        // alert once per day
+
+
+// (ALERT) if caseCount =  90 for case
+    // alert user
+    // prompt reset button, "Ready to change your contact case? This will reset the counter back to zero."
+        // if yes
+            // caseCount = 0
+        // if no 
+            // do nothing
+        // alert once per day
+
+
+
 /*
 //keeps track of left contact lens age (days)
 function lCounter(){
-    //int counter to 0
-    //while running, count++ every 24 hours
-    //at count == 30, send 'alert', ask to reset count to 0?
-    //if glasses is hit, count--
-    //if sleep is hit, count++
-    //if reset is hit, count == 0
+    int counter to 0
+    while running, count++ every 24 hours
+    at count == 30, send 'alert', ask to reset count to 0?
+    if glasses is hit, count--
+    if sleep is hit, count++
+    if reset is hit, count == 0
 }
 
 //keeps track of right contact lens age (days)
